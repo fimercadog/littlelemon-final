f7ed03f9ee4bb8f1a452e071a25d0ff615e5cd7e
fimercadog <fimercadog@gmail.com>
2026-01-16 20:20:51 -0500

    Secure booking API with token authentication

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
 
-from .models import *
-from .serializers import *
+from .models import Menu, Booking
+from .serializers import UserSerializer, MenuSerializer, BookingSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -25,10 +25,7 @@ class SingleMenuItemView(
     serializer_class = MenuSerializer
 
 
-from rest_framework import viewsets
-from .models import Booking
-from .serializers import BookingSerializer
-
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]   # ← ESTE ERA EL PASO CLAVE
